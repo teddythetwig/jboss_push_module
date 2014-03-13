c7e525dc4a20bcfd493c748a65cc058d784a7975
teddythetwig <teddythetwig@gmail.com>
2014-03-13 11:01:03 -0700

    added quartz

---
+++ src/main/MetricJob.java
@@ -0,0 +1,10 @@
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+public class Metric implements Job {
+
+    public void execute(JobExecutionContext context)
+    throws JobExecutionException {
+        //Get the metrics here
+    }
+}
---
+++ src/main/TriggerRunner.java
@@ -0,0 +1,29 @@
+package com.mkyong.quartz;
+ 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class TriggerRunner {
+    public static void main(String[] args) {
+        JobDetail job = JobBuilder.newJob(MetricJob.class)
+            .withIdentity("metricsJob","jbossMetrics")
+            .build();
+        //How should this interval value be passed???
+        Trigger trigger = TriggerBuilder
+            .newTrigger()
+            .withIdentity("metricsTrigger","jbossMetrics")
+            .withSchedule(
+                          SimpleScheduleBuilder.simpleSchedule()
+                          .withIntervalInSeconds(60).repeatForever())
+            .build();
+
+        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+        scheduler.start();
+        scheduler.scheduleJob(job,trigger);
+    }
+}
